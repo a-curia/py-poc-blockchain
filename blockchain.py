4f73b21d2939ffcbd31200c5c79896fc89eabdf7
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2020-02-10 15:51:18 +0200

    finish complex data structures

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 # initializing you blockchain list
+MINING_REWARD = 10 # given to the person who creates a new block
 genesis_block = {
     "previous_hash": '',
     "index": 0,
@@ -7,7 +8,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = "Adrian"
-participants = {"Max"} # set()
+participants = {"Max"}  # set()
 
 
 def get_last_blockchain_value():
@@ -18,6 +19,16 @@ def get_last_blockchain_value():
         return None
     return blockchain[-1]
 
+def verify_transaction(transaction):
+
+    # print(transaction)
+    # print(get_balance(transaction["sender"]))
+
+    sender_balance = get_balance(transaction["sender"])
+
+    return sender_balance >= transaction["amount"]
+
+
 
 def add_transaction(sender, recipient, amount=1.0):
     """
@@ -27,10 +38,12 @@ def add_transaction(sender, recipient, amount=1.0):
     :return: append a new value as well as the last blockchain value to the blockchain
     """
     transaction = {"sender": sender, "recipient": recipient, "amount": amount}
-    open_transactions.append(transaction)
-
-    participants.add(sender)
-    participants.add(recipient)
+    if verify_transaction(transaction): # use not to check the tansactions validity
+        open_transactions.append(transaction)
+        participants.add(sender)
+        participants.add(recipient)
+        return True
+    return False
 
 
 def mine_block():
@@ -39,16 +52,34 @@ def mine_block():
 
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    print("hashed_block = "+hashed_block+"\n")
 
-    # for key in last_block:
-    #     value = last_block[key]
-    #     hashed_block = hashed_block + str(value)
-
-    print(hashed_block)
-    print("\n")
-
-    block = {"previous_hash": hashed_block, "index": len(blockchain), "transactions": open_transactions}
+    reward_transaction = {"sender": "MINING","recipient": owner, "amount": MINING_REWARD}
+    copied_transacions = open_transactions[:]
+    copied_transacions.append(reward_transaction)
+    block = {"previous_hash": hashed_block, "index": len(blockchain), "transactions": copied_transacions}
     blockchain.append(block)
+    return True  # so set the opne_transactions to []
+
+
+def get_balance(participant):
+    tx_sender = [[tx["amount"] for tx in block['transactions'] if tx["sender"] == participant] for block in blockchain]
+    open_tx_sender = [tx["amount"] for tx in open_transactions if tx["sender"] == participant]
+    tx_sender.append(open_tx_sender)
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+
+    tx_recipient = [[tx["amount"] for tx in block['transactions'] if tx["recipient"] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+
+    return amount_received - amount_sent
+
+# reward for miners
 
 
 def get_transaction_value():
@@ -92,40 +123,19 @@ def verify_chain():
 
         if block["previous_hash"] != hash_block(blockchain[index - 1]):
             return False
+
     return True
 
-    # block_index = 0
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
     # is_valid = True
-    #
-    # # for block_index in range(len(blockchain)):
-    # #     if block_index == 0:
-    # #         continue
-    # #     elif blockchain[block_index][0] == blockchain[block_index - 1]:
-    # #         is_valid = True
-    # #     else:
-    # #         is_valid = False
-    #
-    # for block in blockchain:
-    #     print(block)
-    #     print(blockchain[block_index -1])
-    #
-    #     if block_index == 0:
-    #         block_index = block_index + 1
-    #         continue
-    #
-    #     if block[0] == blockchain[block_index - 1]:
+    # for tx in open_transactions:
+    #     if verify_transaction(tx):
     #         is_valid = True
     #     else:
     #         is_valid = False
-    #         break
-    #     block_index += 1
-    #
-    # return  is_valid
+    # return is_valid
 
-
-# # testing it up
-# tx_amount = get_transaction_value()
-# add_transaction(tx_amount)
 
 
 waiting_for_input = True
@@ -136,21 +146,32 @@ while waiting_for_input:
     print("2. Mine a new block")
     print("3. Output the blockchain blocks")
     print("4. Output participants")
+    print("5. Check transaction validity")
     print("h. Manipulate  the chain")
     print("q. Quit")
     user_choice = get_user_choice()
 
     if user_choice == '1':
         tx_data = get_transaction_value()
-        sender, recipient, amount = tx_data
-        add_transaction(sender, recipient, amount)
+        print(tx_data)
+        (sender, recipient, amount) = tx_data
+        if add_transaction(sender,recipient,amount):
+            print("\nTransaction added!")
+        else:
+            print("\nTransaction FAILED!")
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_emelents()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print("All transactions are valid!")
+        else:
+            print("There are INVALID transactions!")
     elif user_choice == 'h':
         # manipulate the blockchain
         if len(blockchain) >= 1:
@@ -166,18 +187,11 @@ while waiting_for_input:
         print("Invalid input option")
 
     if not verify_chain():
+        print_blockchain_emelents()
         print("Invalid blockchain")
         break
-    print("choice register with continue instead of break")
+    print(get_balance("Adrian"))
 else:
     print("While loop is done. Same for for loop!")
-
-    # get_transaction_value()
-    # add_value(tx_amount, get_last_blockchain_value())
-
-    # # Output the blockchain list to the console
-    # for block in blockchain:
-    #     print("Outputting Block >> ")
-    #     print(block)
 
 print("Done!")
